5abc9744536adf7f3493abe1d9f91b0d3a568601
Luis Zhang <luis.zhang@Live.com.au>
2024-07-25 12:26:49 +1000

    Create components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0D47A1",
+    },
+    secondary: {
+      main: "#42A5F5",
+    },
+    background: {
+      default: "#212121",
+      paper: "#424242",
+    },
+    text: {
+      primary: "#FFFFFF",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* <Header /> */}
+      <Container>
+        {/* <Home /> */}
+        {/* <Projects />
+        <Skills />
+        <Testimonials />
+        <Contact /> */}
+      </Container>
+      {/* <Footer /> */}
+    </ThemeProvider>
   );
 }
 
