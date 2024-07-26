dd6538a60b87ccba81f75f0b0efa0f3117879e04
Luis Zhang <luis.zhang@Live.com.au>
2024-07-26 15:46:54 +1000

    Add social media icon links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { Box, Typography, Avatar, Grid, Link } from "@mui/material";
+import { Box, Typography, Avatar, Grid, Link, IconButton } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const Home = () => {
   return (
@@ -14,29 +17,51 @@ const Home = () => {
             </Box>
             <Box mb={4}>
               <Typography variant="h2" gutterBottom>
-                AI Consultant & Frontend Developer
+                AI Engineer & Frontend Developer
               </Typography>
             </Box>
             <Box mb={10}>
               <Typography variant="body1" paragraph>
-                I specialize in creating AI voice assistants for reception
-                duties and have extensive experience as a frontend developer.
+                I specialise in seamlessly integrating AI into business
+                operations and have extensive experience as a frontend
+                developer.
               </Typography>
             </Box>
             <Grid container spacing={2}>
               <Grid item>
-                <Link href="#" color="inherit">
-                  LinkedIn
+                <Link
+                  href="https://www.linkedin.com/in/luis-zhang-a45a1b183/"
+                  color="inherit"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <IconButton color="primary">
+                    <LinkedInIcon fontSize="large" />
+                  </IconButton>
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" color="inherit">
-                  GitHub
+                <Link
+                  href="https://github.com/luisz2k"
+                  color="inherit"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <IconButton color="primary">
+                    <GitHubIcon fontSize="large" />
+                  </IconButton>
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" color="inherit">
-                  Twitter
+                <Link
+                  href="https://www.youtube.com/channel/UCd8HJ1dH1CoNZqu388Opn-w"
+                  color="inherit"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <IconButton color="primary">
+                    <YouTubeIcon fontSize="large" />
+                  </IconButton>
                 </Link>
               </Grid>
             </Grid>
