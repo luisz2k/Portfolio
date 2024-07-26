73bb5e2d1dcc2732d6f27d0aaae9258696d8ad89
Luis Zhang <luis.zhang@Live.com.au>
2024-07-26 17:01:05 +1000

    Update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,26 +3,32 @@ import { Box, Typography, Chip, Grid } from "@mui/material";
 
 const skills = [
   "Python",
+  "C",
+  "HTML",
+  "CSS",
   "JavaScript",
+  "C#",
+  "Java",
   "React.js",
   "Node.js",
-  "Material-UI",
-  "Vapi AI",
-  "Twilio",
-  "Zoho CRM",
-  "Dialogflow",
 ];
 
 const Skills = () => {
   return (
     <Box my={4}>
-      <Typography variant="h4" gutterBottom align="center">
+      <Typography variant="h2" gutterBottom align="center" mb={8}>
         Skills
       </Typography>
       <Grid container spacing={2} justifyContent="center">
         {skills.map((skill, index) => (
           <Grid item key={index}>
-            <Chip label={skill} color="primary" />
+            <Chip
+              label={skill}
+              color="primary"
+              sx={{
+                padding: "5px",
+              }}
+            />
           </Grid>
         ))}
       </Grid>
